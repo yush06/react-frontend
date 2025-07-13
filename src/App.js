51d1f14eb21876f08f57eb724d95b93421498611
yush06 <ayushsaxena0612@gmail.com>
2025-07-13 23:53:18 +0530

    mychanges

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to ReactJS World234567999</p>
+        <p>Welcome to ReactJS </p>
         <a
           className="App-link"
           href="https://reactjs.org"
