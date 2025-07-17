230d6d40a1333cc5ad0f0fef8ffd8a9cfde10760
yush06 <ayushsaxena0612@gmail.com>
2025-07-17 23:19:49 +0530

    1607

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to ReactJS 12444</p>
+        <p>Welcome to ReactJS </p>
         <a
           className="App-link"
           href="https://reactjs.org"
